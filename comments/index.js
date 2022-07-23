92f42f59766538ffe9fadd7f82dd3c0f54f0ff64
Prateek Varma <prtk.varma@gmail.com>
2022-07-23 17:22:12 -0600

    Created microservice for 'comments', data saved in memory - testable from postman

---
+++ comments/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {}; //Temporary object to store in memory comments
+
+app.get("/posts/:id/comments", (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || []; //checks if any comments were aready present, associoated with this id. Else assign an empty array.
+  comments.push({ id: commentId, content }); //inserts object into comments variable
+  commentsByPostId[req.params.id] = comments; // inserts a new comment object inside commentsByPostId as an array
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Listening on 4001");
+});
