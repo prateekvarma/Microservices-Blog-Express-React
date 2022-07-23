59f7848530fc5ba2f7058bac036ec433f409532c
Prateek Varma <prtk.varma@gmail.com>
2022-07-23 16:21:17 -0600

    Created 'posts' microservice, data saved in memory - testable with postman

--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
 const app = express();
+app.use(bodyParser.json());
 
-const posts = {};
+const posts = {}; //just a temp object, to act as a memory storage.
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  
+  posts[id] = { id, title }; //save the post as a new key-value pair (in memory)
+  
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log("Listening on 4000");
