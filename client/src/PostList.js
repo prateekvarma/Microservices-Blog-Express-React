b8c72cc6fc185a7f5c4013febb19cec24d024f9c
Prateek Varma <prtk.varma@gmail.com>
2022-07-24 17:04:10 -0600

    Created comments functionality, and displayed them according to the post. (might have a warning on terminal)

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -18,6 +20,8 @@ const PostList = () => {
       <div className="card" key={post.id}>
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
