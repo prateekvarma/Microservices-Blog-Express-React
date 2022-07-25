52336ad0363c5015d84e949ffaa4b5f2874c7245
Prateek Varma <prtk.varma@gmail.com>
2022-07-24 18:35:35 -0600

    created new microservice for an 'Event Bus' - to track changes in sub microservices

---
+++ event-bus/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+//The SOLO job of this event bus is just to emit events to all sub apps, as soon as it receives an event.
+
+app.post("/events", (req, res) => {
+  const event = req.body; // body could be any data - json, string, num. We don't know what because multiple microservices will make POST calls to it.
+
+  //Forward event calls to all sub services
+  axios.post("http://localhost:4000/events", event).catch((error) => {
+    console.log(error.message);
+  }); //posts service
+  axios.post("http://localhost:4001/events", event).catch((error) => {
+    console.log(error.message);
+  }); //comments service
+  axios.post("http://localhost:4002/events", event).catch((error) => {
+    console.log(error.message);
+  }); //Query service
+
+  res.send({ status: ok });
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
