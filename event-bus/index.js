0aa2d8e46baf851d44ea01a8c4d7d54ef231b49c
Prateek Varma <prtk.varma@gmail.com>
2022-07-26 16:41:16 -0600

    step1 for implementing event sync: Store all incoming events inside the Event Bus, and create a GET route to serve all those events

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,9 +6,14 @@ const app = express();
 app.use(bodyParser.json());
 
 //The SOLO job of this event bus is just to emit events to all sub apps, as soon as it receives an event.
+//The 2nd job of this event bus now, is to store ALL incoming events in a local variable. Doing this will ensure, that went another microservice goes down, this service can serve them when they're back.
+
+const events = []; //this is the local in memory variable that will store all events.
 
 app.post("/events", (req, res) => {
   const event = req.body; // body could be any data - json, string, num. We don't know what because multiple microservices will make POST calls to it.
+
+  events.push(event); //take every event, and push inside the local store.
 
   //Forward event calls to all sub services
   axios.post("http://localhost:4000/events", event).catch((error) => {
@@ -26,6 +31,11 @@ app.post("/events", (req, res) => {
 
   res.send({ status: "ok" });
 });
+
+app.get("/events", (req, res) => {
+  //whenever anyone requests events from this endpoint, get ALL the events from the local in memory variable.
+  res.send(events);
+})
 
 app.listen(4005, () => {
   console.log("Listening on 4005");
